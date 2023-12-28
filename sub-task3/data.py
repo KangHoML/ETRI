94feb457bd71f89564aec0e2426cd2744fd9898b
KangHoML <kanghokhkh1@gmail.com>
2023-12-29 02:10:59 +0900

    need to make dataset class

---
+++ sub-task3/data.py
@@ -0,0 +1,17 @@
+from torch.utils.data import Dataset
+
+class FashionDataset(Dataset):
+    def __init__(self):
+
+
+    def __len__(self):
+        return
+    
+
+    def __getitem__(self, index):
+        return
+    
+
+if __name__ == "__main__":
+    example_dataset = FashionDataset()
+    print(f'Length of data : {len(example_dataset)}')
